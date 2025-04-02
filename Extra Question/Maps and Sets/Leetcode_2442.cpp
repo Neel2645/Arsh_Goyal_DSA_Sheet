023869f20e209f28d2aea3be2be4c35b1eccde43
use <sathvaraneel52@gmail.com>
2025-04-03 02:24:28 +0530

    leetcode_2442

---
+++ Extra Question/Maps and Sets/Leetcode_2442.cpp
@@ -0,0 +1,32 @@
+//! Leetcode - 2442 :- Count Number of Distinct Integers After Reverse Operations
+
+//* Method - 1
+//? TC - O(N)
+//? SC - O(N)
+
+class Solution {
+    public:
+    
+        int reverse(int n)
+        {
+            int ans = 0;
+            while(n)
+            {
+                int d = n % 10;
+                ans = ans * 10 + d;
+                n /= 10;
+            }
+            return ans;
+        }
+    
+        int countDistinctIntegers(vector<int>& nums) {
+            unordered_set<int> s;
+    
+            for(int &ele : nums)
+            {
+                s.insert(ele);
+                s.insert(reverse(ele));
+            }
+            return s.size();
+        }
+};
